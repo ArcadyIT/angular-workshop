34a15b8472970fc8fa85039231ab9895d373f2d7
Peter Tangelder <p.tangelder@ijsseltechnologie.nl>
2018-10-24 11:14:42 +0200

    add calculator

--- testableapp/src/app/app.module.ts
+++ testableapp/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MessageComponent } from './message/message.component';
 import { NotebookComponent } from './notebook/notebook.component';
 import { ProductOverviewComponent } from './product-overview/product-overview.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessageComponent,
     NotebookComponent,
-    ProductOverviewComponent
+    ProductOverviewComponent,
+    CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
