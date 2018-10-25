98bce46caf73b14466ebf19d042445bcbbd4749a
Peter Tangelder <p.tangelder@ijsseltechnologie.nl>
2018-10-25 14:57:09 +0200

    Remove unused import

--- recipeace/src/app/recipe/recipe.module.ts
+++ recipeace/src/app/recipe/recipe.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule, MatButton } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeCardComponent } from './recipe-card/recipe-card.component';
@@ -16,7 +16,8 @@ import { RecipeCardComponent } from './recipe-card/recipe-card.component';
   ],
   declarations: [
     RecipeListComponent,
-    RecipeCardComponent],
+    RecipeCardComponent
+  ],
   exports: [RecipeListComponent]
 })
 export class RecipeModule { }
